16eec406bef7a1a01ca1131f94abd051b256aa72
Martin Lehmann <martin@lehmann.tech>
2025-03-25 11:39:12 +0100

    extract the config things again

--- project/index.ts
+++ project/index.ts
@@ -4,18 +4,23 @@ import {Config, getStack} from "@pulumi/pulumi";
 const env = getStack();
 const config = new Config()
 
+const region = config.require('region');
+const nodePoolName = "default";
+const nodeCount =config.requireNumber('nodeCount')
+const version = "1.32.2-do.0";
+
 const doCluster = new digitalocean.KubernetesCluster("do-cluster", {
   name: `martin-pug-oslo-${env}`,
-  region: config.require('region'),
-  version: "1.32.2-do.0",
+  region,
+  version,
   destroyAllAssociatedResources: true,
   tags: [getStack(), 'typescript'],
   nodePool: {
-    name: "default",
+    name: nodePoolName,
     size: "s-2vcpu-2gb",
-    nodeCount: config.requireNumber('nodeCount'),
+    nodeCount,
   },
 });
-export const name = doCluster.name;
+export const { name } = doCluster;
 export const kubeconfig = doCluster.kubeConfigs.apply(kubeConfigs => kubeConfigs[0].rawConfig);
 
