d0d8d8388af7baa11442d7fd8c55e77f75ea8256
Martin Lehmann <martin@lehmann.tech>
2025-03-25 10:56:20 +0100

    woo!

--- project/index.ts
+++ project/index.ts
@@ -1,10 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as digitalocean from "@pulumi/digitalocean";
 
-// Create a DigitalOcean resource (Domain)
-const domain = new digitalocean.Domain("my-domain", {
-  name: "my-domain.io"
+const doCluster = new digitalocean.KubernetesCluster("do-cluster", {
+  name: 'martin-pug-oslo',
+  region: "fra1",
+  version: "1.32.2-do.0",
+  destroyAllAssociatedResources: true,
+  nodePool: {
+    name: "default",
+    size: "s-2vcpu-2gb",
+    nodeCount: 1,
+  },
 });
+export const name = doCluster.name;
+export const kubeconfig = doCluster.kubeConfigs.apply(kubeConfigs => kubeConfigs[0].rawConfig);
 
-// Export the name of the domain
-export const domainName = domain.name;
