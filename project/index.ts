02d275ac389201646341fa1478cfbb311832e37b
Martin Lehmann <martin@lehmann.tech>
2025-03-25 11:06:51 +0100

    prod

--- project/index.ts
+++ project/index.ts
@@ -1,14 +1,19 @@
 import * as digitalocean from "@pulumi/digitalocean";
+import {Config, getStack} from "@pulumi/pulumi";
+
+const env = getStack();
+const config = new Config()
 
 const doCluster = new digitalocean.KubernetesCluster("do-cluster", {
-  name: 'martin-pug-oslo',
-  region: "fra1",
+  name: `martin-pug-oslo-${env}`,
+  region: config.require('region'),
   version: "1.32.2-do.0",
   destroyAllAssociatedResources: true,
+  tags: [getStack(), 'typescript'],
   nodePool: {
     name: "default",
     size: "s-2vcpu-2gb",
-    nodeCount: 1,
+    nodeCount: config.requireNumber('nodeCount'),
   },
 });
 export const name = doCluster.name;
